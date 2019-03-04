868103a98385f41c86aefee65cbc4d51d90bafa4
Natalie Marion <asuahsahua@users.noreply.github.com>
2019-03-04 08:43:22 -0500

    Rearrange into front/back

--- js/scripts.js
+++ js/scripts.js
@@ -34,20 +34,52 @@ jQuery(function() {
             cards.push(current.trim());
         }
 
+        switch (cards.length % 3) {
+            case 0:
+                break;
+            case 1:
+                cards.push("");
+                cards.push("");
+                break;
+            case 2:
+                cards.push("");
+                break;
+        };
+
         cards.forEach(addCard);
+
+        // rearrange the cards so we can print front and back
+        var front = $(".front");
+        var back = $(".back");
+        var cardsDiv = $("#cards");
+
+        // chunk by 3 both ways
+        var frontChunks = _(front).chunk(3);
+        var backChunks = _(back).chunk(3);
+
+        // Redistribute
+        cardsDiv.html("");
+        for (i = 0; i < frontChunks.length; i++) {
+            frontChunks[i].forEach(function(element) {
+                cardsDiv.append(element);
+            });
+            backChunks[i].forEach(function(element) {
+                cardsDiv.append(element);
+            });
+        }
     }
 
     function addCard(card) {
         var lines1 = card.split('\n');
         var lines2 = [];
 
-        var card1Container = $(`<div class="card-container"></div>`);
-        var card1 = $(`<div class="card front"></div>`);
+        var card1Container = $(`<div class="card-container front"></div>`);
+        var card1 = $(`<div class="card"></div>`);
         card1.appendTo(card1Container);
         $("#cards").append(card1Container);
 
-        var card2Container = $(`<div class="card-container"></div>`);
-        var card2 = $(`<div class="card back"></div>`);
+        var card2Container = $(`<div class="card-container back"></div>`);
+        var card2 = $(`<div class="card"></div>`);
         card2.appendTo(card2Container);
         $("#cards").append(card2Container);
 
